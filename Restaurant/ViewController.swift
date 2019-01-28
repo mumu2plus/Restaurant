3f242a23c26ae52d240388484f02ac9206f3819d
mumu2plus <mumu2plus@gmail.com>
2019-01-29 03:07:06 +0800

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by 加加林 on 2019/1/29.
+//  Copyright © 2019 mumu2plus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
